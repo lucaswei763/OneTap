929d4990e6ccbd692b46a9a50828e8c12d23b683
lucaswei763 <2787322134@qq.com>
2026-09-15 23:20:39 +0800

    Initial Commit

---
+++ OneTap/OneTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneTapApp.swift
+//  OneTap
+//
+//  Created by 韦亦航 on 2026/9/15.
+//
+
+import SwiftUI
+
+@main
+struct OneTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
